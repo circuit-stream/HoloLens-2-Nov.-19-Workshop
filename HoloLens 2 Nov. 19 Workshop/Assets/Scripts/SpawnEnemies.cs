67b825c12f020ba92beeac322a238b431c90e7c4
nakisa <nakisa.donnelly@gmail.com>
2020-11-19 14:44:34 -0700

    Added Unity project

---
+++ HoloLens 2 Nov. 19 Workshop/Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject enemyPrefab;
+
+    void Start()
+    {
+        StartCoroutine(TimedRaycast());
+    }
+    
+
+    private IEnumerator TimedRaycast()
+    {
+        while (true)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(mainCamera.transform.position, RandomDirection(), out hit))
+            {
+                SpawnTheThing(hit.point);
+                yield return new WaitForSeconds(5f);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+    public void SpawnTheThing(Vector3 position)
+    {
+        GameObject spawnedEnemy = Instantiate(enemyPrefab, position, Quaternion.identity);
+    }
+
+    private Vector3 RandomDirection()
+    {
+        return new Vector3(Random.RandomRange(0, 1), Random.Range(0.5f, mainCamera.transform.position.y + 0.4f), Random.Range(0, 1));
+    }
+}
